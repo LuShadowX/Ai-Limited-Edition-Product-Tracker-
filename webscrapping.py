401f87c79ba01e11607e98ef445beedca420a7a5
LuShadowX <supyallukshyadl1@gmail.com>
2026-02-12 18:30:17 +0530

    adding other things

--- webscrapping.py
+++ webscrapping.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import smtplib
+from dotenv import load_dotenv
+load_dotenv()
 def getresult(url):
     headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.9"}
@@ -17,7 +19,7 @@ def getresult(url):
     if price_element:
          price=price_element.get_text().strip()
          clean_price=price.replace(".","").replace(",","")
-         current_price=float(price)
+         current_price=float(clean_price)
          print(f"The price of the product is: {current_price}")
          budget=1000
          if current_price<=budget:
@@ -34,8 +36,8 @@ def getresult(url):
         print("Not avaliable")
         print(response.text[:100])
 def send_email(product_title,current_price,url):
-    my_email="supyallukshyadl1@gmail.com"
-    my_password="chkg ysvm xhse qjrs "
+    my_email=os.getenv("My_Email")
+    my_password=os.getenv("My_Password")
     receiver_address="supyallukshyadl1@gmail.com"
     message = f"Subject: LOW PRICE ALERT!\n\nHi! The price for {product_title} has dropped to {current_price}.\n\nBuy it here: {url}"
     with smtplib.SMTP("smtp.gmail.com",587) as connection:
@@ -49,6 +51,7 @@ def send_email(product_title,current_price,url):
          print("EMAIL SENT SUCCESSFULLY!")
          
 
-getresult("https://amzn.in/d/0c5YDIAN")
+product_to_track=["https://amzn.in/d/06Pwbey5","https://amzn.in/d/0bDOcukR","https://amzn.in/d/0ik1OB5g"]
+for link in product_to_track:
+    getresult(link)
 
-    
