f3acc3d6b63f5b309658afd162c9c274ceeb7e37
LuShadowX <supyallukshyadl1@gmail.com>
2026-02-13 15:07:20 +0530

    adding new things

--- webscrapping.py
+++ webscrapping.py
@@ -22,8 +22,14 @@ def save_to_csv(product_name,price,link):
         print("Data save to the history")
 
 def getresult(url):
-    headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Accept-Language": "en-US,en;q=0.9"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Language": "en-US,en;q=0.9,hi;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1"
+    }
     
     try:
         response=requests.get(url,headers=headers,verify=False)
